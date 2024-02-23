5af97e890138dc5614c921ede84048a100a0b2d8
reichert621 <reichertjalex@gmail.com>
2024-02-23 13:25:13 -0500

    Add other animation hook

--- utils/animation.ts
+++ utils/animation.ts
@@ -37,3 +37,35 @@ export function useScaleAnimation({
 
   return [animatedStyle, { onPressIn, onPressOut }];
 }
+
+export function useOpacityAnimation({
+  config = { damping: 10, stiffness: 100 },
+  range = [1, 0.95],
+}: {
+  config?: WithSpringConfig;
+  range?: [number, number];
+}): [any, { onPressIn: () => void; onPressOut: () => void }] {
+  const scale = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const value = interpolate(
+      scale.value,
+      [0, 1], // input range: corresponds to scale.value range
+      range
+    );
+
+    return {
+      opacity: value,
+    };
+  });
+
+  const onPressIn = () => {
+    scale.value = withSpring(1, config);
+  };
+
+  const onPressOut = () => {
+    scale.value = withSpring(0, config);
+  };
+
+  return [animatedStyle, { onPressIn, onPressOut }];
+}
