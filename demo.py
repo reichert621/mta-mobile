315646e6b3c0d40a640cd8d33c1ba5386e8df548
Alex Reichert <reichertjalex@gmail.com>
2024-03-02 18:19:51 -0500

    This is a demo commit using the API

--- demo.py
+++ demo.py
@@ -15,5 +15,31 @@ def hello():
     name = request.args.get('name', 'World')
     return jsonify({'message': f'Hello, {name}!'})
 
+@app.route('/api/add', methods=['GET'])
+def add():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+    return jsonify({'result': num1 + num2})
+
+@app.route('/api/subtract', methods=['GET'])
+def subtract():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+    return jsonify({'result': num1 - num2})
+
+@app.route('/api/multiply', methods=['GET'])
+def multiply():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+    return jsonify({'result': num1 * num2})
+
+@app.route('/api/divide', methods=['GET'])
+def divide():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+    if num2 == 0:
+        return jsonify({'error': 'Division by zero is not allowed'}), 400
+    return jsonify({'result': num1 / num2})
+
 if __name__ == '__main__':
     app.run(debug=True)
