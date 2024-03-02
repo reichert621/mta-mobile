84ac8862490dafae735dfc63dd77cce056e5d48e
Alex Reichert <reichertjalex@gmail.com>
2024-03-02 13:20:41 -0500

    This is a demo commit using the API

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
-# TODO: implement me!
+def fibonacci(n):
+    "generate first N fibonacci numbers"
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence[:n]
