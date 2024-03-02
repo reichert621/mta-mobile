96b1bf33fd7a5b9ea0d27bef1c76fd55ffc38362
Alex Reichert <reichertjalex@gmail.com>
2024-03-02 13:26:39 -0500

    This is a demo commit using the API

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 def fibonacci(n):
     "generate first N fibonacci numbers"
 
@@ -5,3 +9,11 @@ def fibonacci(n):
     while len(fib_sequence) < n:
         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
     return fib_sequence[:n]
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    name = request.args.get('name', 'World')
+    return jsonify({'message': f'Hello, {name}!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
