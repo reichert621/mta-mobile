d36ff185cde2085bee8f35cd4dcafddcd8339794
Alex Reichert <reichertjalex@gmail.com>
2024-03-02 22:13:12 -0500

    This is a demo commit using the API

--- calculator.py
+++ calculator.py
@@ -1,7 +1,17 @@
 
+import math
+
 def add(x, y):
     "Add two numbers"
     return x + y
+
+def power(x, y):
+    "Calculate the power of a number"
+    return math.pow(x, y)
+
+def sqrt(x):
+    "Calculate the square root of a number"
+    return math.sqrt(x)
 
 def subtract(x, y):
     "Subtract two numbers"
